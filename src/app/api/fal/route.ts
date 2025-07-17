dedaaf2a404b530414a097375d2f731efda979b4
Noah Solomon <noahsolomon2003@gmail.com>
2025-07-16 21:04:53 -0700

    format

--- src/app/api/fal/route.ts
+++ src/app/api/fal/route.ts
@@ -43,7 +43,7 @@ export const POST = async (req: NextRequest) => {
                   : "100",
             "X-RateLimit-Period": limiterResult.period,
           },
-        }
+        },
       );
     }
   }
